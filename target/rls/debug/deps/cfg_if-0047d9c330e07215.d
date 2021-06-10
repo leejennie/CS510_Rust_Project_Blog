c565ff9e5cabfaf082ca025ef61f44621d489ee2
Jennie <ljennie@pdx.edu>
2021-06-09 21:23:24 -0700

    testing out pages again

--- target/rls/debug/deps/cfg_if-0047d9c330e07215.d
+++
@@ -1,5 +0,0 @@
-/Users/jennielee/Documents/PSU 2020-2021/SPRING2021/CS510/yew-app/target/rls/debug/deps/cfg_if-0047d9c330e07215.rmeta: /Users/jennielee/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/Users/jennielee/Documents/PSU 2020-2021/SPRING2021/CS510/yew-app/target/rls/debug/deps/cfg_if-0047d9c330e07215.d: /Users/jennielee/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/Users/jennielee/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs:
